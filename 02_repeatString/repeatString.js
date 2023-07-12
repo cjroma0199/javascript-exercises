ca94f745d510bb16f74d0038b6472f2e181c984f
cjrdelosreyes <122694227+cjroma0199@users.noreply.github.com>
2023-07-12 12:47:22 +0800

    finish reapeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (word, repeat) {
+  let repeatedWord = word;
 
+  if (repeat < 0) return "ERROR";
+  if (repeat == 0) return "";
+
+  for (let i = 1; i < repeat; i++) {
+    repeatedWord += word;
+  }
+  return repeatedWord;
 };
 
 // Do not edit below this line
