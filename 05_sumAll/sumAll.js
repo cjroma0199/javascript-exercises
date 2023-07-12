d1d2851224c548fabc071da442bfe5ec237684ee
cjrdelosreyes <122694227+cjroma0199@users.noreply.github.com>
2023-07-12 17:26:57 +0800

    finish sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (numberOne, numberTwo) {
+  if (
+    !Number.isInteger(numberOne) ||
+    !Number.isInteger(numberTwo) ||
+    numberOne < 0 ||
+    numberTwo < 0
+  ) {
+    return "ERROR";
+  }
+  let higher = numberOne;
+  let lower = numberTwo;
+  let sum = 0;
 
+  if (numberOne < numberTwo) {
+    higher = numberTwo;
+    lower = numberOne;
+  }
+
+  for (; lower <= higher; lower++) {
+    sum += lower;
+  }
+  return sum;
 };
 
 // Do not edit below this line
