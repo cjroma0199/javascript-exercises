a91f3bb0892c605f2445602cb390b8dfb288c603
cjrdelosreyes <122694227+cjroma0199@users.noreply.github.com>
2023-07-30 06:57:57 +0800

    Complete JavaScript exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanString = str.replaceAll(/(\p{P}|\p{Z})/gu, '').toLowerCase();
+  return cleanString === cleanString.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const currentYear = new Date().getFullYear();
+  return array
+    .sort((personOne, personTwo) => {
+      if (!personOne.yearOfDeath) personOne.yearOfDeath = currentYear;
+      if (!personTwo.yearOfDeath) personTwo.yearOfDeath = currentYear;
 
+      return (
+        personOne.yearOfDeath -
+        personOne.yearOfBirth -
+        (personTwo.yearOfDeath - personTwo.yearOfBirth)
+      );
+    })
+    .pop();
 };
 
 // Do not edit below this line
